0843802aca1ead7628f60f2052a73ba97a8ad6f3
saloniurkude414 <saloniurkude414@gmail.com>
2026-03-27 20:42:22 +0530

    23

---
+++ ChatApplication.java
@@ -0,0 +1,16 @@
+class ChatApplication {
+
+    public static void main(String[] args) {
+
+        Thread sender = new Thread(() -> {
+            System.out.println("Sending message...");
+        });
+
+        Thread receiver = new Thread(() -> {
+            System.out.println("Receiving message...");
+        });
+
+        sender.start();
+        receiver.start();
+    }
+}
